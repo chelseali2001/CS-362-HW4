99239dd58dcc0196bf9fc7bdf6f8831cff87a321
Chelsea Li <chelseali2001@gmail.com>
2021-04-29 16:18:53 -0700

    modified tests

--- test_average.py
+++ test_average.py
@@ -4,7 +4,11 @@ import average
 class SimpleTest(unittest.TestCase):
     def test_average(self):
         self.assertEqual(average.avg([2, 5, 2]), 3.0) 
+    
+    def test_average0(self):
         self.assertEqual(average.avg([]), 0.0)
+    
+    def test_average1(self):
         self.assertEqual(average.avg([0, 0, 0]), 0.0)
 
 if __name__ == '__main__':
