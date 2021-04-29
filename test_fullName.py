f14609d4d5e6a0bf6b22fb84709314b05be67b3f
Chelsea Li <chelseali2001@gmail.com>
2021-04-29 16:19:36 -0700

    changed volume program and updated its test program

--- test_fullName.py
+++ test_fullName.py
@@ -4,7 +4,11 @@ import fullName
 class SimpleTest(unittest.TestCase):
     def test_fullName(self):
         self.assertEqual(fullName.full_name("Chelsea", "Li"), "Chelsea Li") 
+        
+    def test_fullName0(self):
         self.assertEqual(fullName.full_name ("Chelsea", ""), "Chelsea ")
+    
+    def test_fullName1(self):
         self.assertEqual(fullName.full_name ("Chelsea", 1), "Chelsea 1")
 
 if __name__ == '__main__':
